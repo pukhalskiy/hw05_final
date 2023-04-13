25d52a1906785a11c48d8594f0947456def488aa
perineum14 <ego3200@gmail.com>
2023-04-13 23:03:21 +0300

    improve profile_follow

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -131,6 +131,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
+    if author == request.user:
+        return redirect('posts:profile', username)
     following, created = Follow.objects.get_or_create(
         user=request.user,
         author=author
